df32503039077e9609385542b9b6c66304ff8bcf
Ryan Cowie <ryancowie@Ryans-Pro.hsd1.ca.comcast.net>
2019-06-14 02:00:35 -0700

    Adds styling and completes step 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -23,7 +23,7 @@ class App extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className="app-container">
         <Students
           data={this.state.data}
         />
--- client/src/components/children/Students.jsx
+++ client/src/components/children/Students.jsx
@@ -11,18 +11,28 @@ class Students extends React.Component {
     let student = this.props.data.map((person, i) => {
       let average = person.grades.reduce((a, b) => parseInt(a) + parseInt(b)) / person.grades.length;
       return (
-        <div>
+        <div className="student-container">
           <img src={person.pic} className="student-pic" />
-          <div className="student-name">
-            {person.firstName}
-            {'  '}
-            {person.lastName}
-          </div>
-          <div className="student-info">
-            {person.email}
-            {person.company}
-            {person.skill}
-            {`${average}%`}
+          <div className="student-info-container">
+            <div className="student-name">
+              {person.firstName}
+              {'  '}
+              {person.lastName}
+            </div>
+            <div className="student-info">
+              <div>
+                Email: {' '}{person.email}
+              </div>
+              <div>
+                Company: {' '}{person.company}
+              </div>
+              <div>
+                Skills: {' '}{person.skill}
+              </div>
+              <div>
+                Average: {' '}{`${average}%`}
+              </div>
+            </div>
           </div>
         </div>
       )
